4ac6a82ec3166b28593823cf94d898156d5e94e6
Daniel Wallace <danielwallace@gtmanfred.com>
2014-01-02 01:54:10 -0600

    remove redundant auth from challenge3

--- challenge3.php
+++ challenge3.php
@@ -1,23 +1,6 @@
 <?php
 require "auth.php";
 require "check.php";
-use OpenCloud\Rackspace;
-
-
-$ini_array = parse_ini_file(getenv("HOME") . "/.rackspace_cloud_credentials", true);
-
-$client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array(
-    'username' => $ini_array['rackspace']['username'],
-    'apiKey'   => $ini_array['rackspace']['apikey']
-));
-
-$client->authenticate();
-
-try {
-    $region = $ini_array['rackspace']['region'];
-} catch (Exception $e) {
-    $region = 'IAD';
-}
 
 $dns = $client->dnsService();
 $domainList = $dns->domainList();
